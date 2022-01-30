46132db8e46604aea9a979c5f70af8e554742153
Matheus Gomes <matheusdin04@gmail.com>
2022-01-30 14:52:00 -0300

    Showing genres on tv show details screen

--- app/src/main/java/com/matheus/tvmazechallenge/features/tvshowdetails/ui/TVShowDetailsFragment.kt
+++ app/src/main/java/com/matheus/tvmazechallenge/features/tvshowdetails/ui/TVShowDetailsFragment.kt
@@ -6,12 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.matheus.tvmazechallenge.databinding.TvShowDetailsFragmentBinding
 import com.matheus.tvmazechallenge.features.tvshowdetails.viewmodel.TVShowDetailsViewModel
+import com.matheus.tvmazechallenge.shared.base.StateData
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class TVShowDetailsFragment : Fragment() {
 
+    private val tvShowGenreAdapter = TVShowGenreAdapter()
     private val viewModel: TVShowDetailsViewModel by viewModel()
     private val args: TVShowDetailsFragmentArgs by navArgs()
 
@@ -21,14 +24,36 @@ class TVShowDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding = TvShowDetailsFragmentBinding.inflate(inflater, container, false)
-
+        configureBindings(binding)
+        configureTVShowEpisodesListener(binding)
         return binding.root
     }
 
-    private fun configureTVShowDetailsListener(binding: TvShowDetailsFragmentBinding) =
+    private fun configureBindings(binding: TvShowDetailsFragmentBinding) = with(binding) {
+        tvShowDetails = args.tvShow
+        tvShowDetailsRvGenres.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = tvShowGenreAdapter.apply {
+                addItems(args.tvShow.genres)
+            }
+        }
+    }
+
+    private fun configureTVShowEpisodesListener(binding: TvShowDetailsFragmentBinding) =
         with(binding) {
             viewModel.tvShowDetailsResult.observe(viewLifecycleOwner) {
+                when (it) {
+                    is StateData.Success -> {
+                        isLoadingEpisodes = false
 
+                    }
+                    is StateData.Loading -> {
+                        isLoadingEpisodes = true
+                    }
+                    is StateData.Failure -> {
+                        isLoadingEpisodes = false
+                    }
+                }
             }
         }
 
---
+++ app/src/main/java/com/matheus/tvmazechallenge/features/tvshowdetails/ui/TVShowGenreAdapter.kt
@@ -0,0 +1,30 @@
+package com.matheus.tvmazechallenge.features.tvshowdetails.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.matheus.tvmazechallenge.R
+import com.matheus.tvmazechallenge.databinding.TvShowGenreItemLayoutBinding
+import com.matheus.tvmazechallenge.shared.base.BaseAdapter
+import com.matheus.tvmazechallenge.shared.base.BaseViewHolder
+
+class TVShowGenreAdapter : BaseAdapter<String>() {
+    override fun getViewHolder(parent: ViewGroup) = TVShowGenreViewHolder(
+        TvShowGenreItemLayoutBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+    )
+
+    override fun getLayoutRes() = R.layout.tv_show_genre_item_layout
+
+    inner class TVShowGenreViewHolder(private val binding: TvShowGenreItemLayoutBinding) :
+        BaseViewHolder<String>(binding.root) {
+        override fun bind(item: String, position: Int) {
+            binding.apply {
+                tvShowGenreTvGenre.text = item
+                executePendingBindings()
+            }
+        }
+    }
+}
